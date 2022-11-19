b36f129ffd7965779d0224451abf9a3f91545ad8
Wallace Vieira <wallaceofc@hotmail.com>
2022-11-18 21:18:33 -0300

    Criando rota de login e firebase (consertando bug)

--- src/pages/UserProfile.js
+++ src/pages/UserProfile.js
@@ -1,9 +1,40 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../context/authContext";
 
 export const UserProfile = () => {
+  const { currentUser, signOut } = useAuth();
+  const navigate = useNavigate();
+
+  async function handleLogout() {
+    try {
+      await signOut();
+      navigate("/login");
+    } catch (error) {
+      alert("Ocorreu um erro ao tentar efetuar o logout");
+    }
+  }
+
   return (
-    <div>
-      <h1>User Profile</h1>
+    <div className="container">
+      <h1>Perfil do Usuário</h1>
+
+      <button>Sair</button>
+
+      <table>
+        <thead>
+          <tr>
+            <th>Email</th>
+            <th>Ações</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Email: {currentUser?.email}</td>
+            <td>Atualizar perfil do usuário</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 };
